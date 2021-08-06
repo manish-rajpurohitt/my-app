b055e142ed7ae44585cdcbbb2bd31e46d6cf79b0
Manish Purohit <manishkumar9866@gmail.com>
2021-08-06 21:26:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const admin = require('firebase-admin');
 //initialize admin SDK using serciceAcountKey
 let creds = {
     type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
+    projectId: process.env.PROJECT_ID,
     private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.CLIENT_EMAIL,
     client_id: process.env.CLIENT_ID,
     auth_uri: process.env.AUTH_URI,
     token_uri: process.env.TOKEN_URI,
